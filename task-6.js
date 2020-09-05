e00618b7422c8427f2bb02a15c346908dcca1628
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-05 21:13:23 +0300

    hw-01

--- task-6.js
+++ task-6.js
@@ -1,9 +1,6 @@
 let input = confirm("Хотите ввсести число?");
 let total = 0;
 
-// total = prompt();
-// total = ++total;
-
 while (input) {
   input = +prompt("Введитие, пожалуйста число. ");
   total += input;
